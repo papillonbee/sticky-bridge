099a4bba42472e5ddb7955d18b814bdd0a83b6da
papillonbee <37870054+papillonbee@users.noreply.github.com>
2025-02-22 20:47:32 +0000

    Deploying to gh-pages from @ papillonbee/sticky-bridge@971a2266d743d2d5ea8327f0b338e919453065dd 🚀

---
+++ config.js
@@ -0,0 +1,5 @@
+window.CONFIG = {
+  BRIDGE_SERVICE_URL: 'https://bridge-service-713826805353.us-central1.run.app',
+  BRIDGE_SERVICE_REST_PROTOCOL: 'https',
+  BRIDGE_SERVICE_WEB_SOCKET_PROTOCOL: 'wss'
+};
